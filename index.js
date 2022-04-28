de77bb3cd0181a3ff7e2bd70542d00b8ba896827
cadenkarras16 <caden.karras4@gmail.com>
2022-04-28 14:35:38 -0600

    index.js changes. it runs. Weather shows pending promis

--- index.js
+++ index.js
@@ -84,8 +84,8 @@ app.get("/", (req, res) => {
     console.log(type + apiNumber);
 
     weatherResult = axios.get('https://api.open-meteo.com/v1/forecast?latitude=40.25&longitude=-111.66&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver');
-    
-    
+    console.log(weatherResult);
+
     swapi.get('https://swapi.dev/api/' + type + '/' + apiNumber ).then(result => {
         res.render("index", {aStarWars: result, data: type, characterId: apiNumber, aWeatherResult: weatherResult}, );
     }).catch(err => {
