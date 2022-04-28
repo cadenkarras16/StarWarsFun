12562d8255860dd5c63c913cba0b5cf061a7483c
cadenkarras16 <caden.karras4@gmail.com>
2022-04-28 11:14:56 -0600

    adding in pictures and testing

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 
 const swapi = require('swapi-node');
 
+app.use(express.static(__dirname+'/public'));
+
 //swapi.get('https://swapi.dev/api/starships/9').then((result) => {
 //    console.log(result);
 //});
@@ -17,12 +19,13 @@ app.get("/", (req, res) => {
     //this is the random number for the value inside the type of variables we are pulling
     //let apiNumber = Math.floor(Math.random() * 84);
     let apiNumber = 1;
+    
     let typeNumber = 2;
 
 //Commented out below for testing with only people purposes
 
     let data = ["planets","starships", "people", "films", "species"]
-    //let typeNumber = Math.floor(Math.random() * 5);
+    //typeNumber = Math.floor(Math.random() * 5);
     let type = data[typeNumber]
 
 
@@ -38,18 +41,18 @@ app.get("/", (req, res) => {
     {
         //errors happen on the following numbers from the API: 17, 
         //I have only gone up to 50 as of April 27th ^^
-        apiNumber = Math.floor(Math.random() * 84);
+        //apiNumber = Math.floor(Math.random() * 84);
         if (apiNumber == 17) {
             apiNumber = 1
         }
     }
     else if (type == "films")
     {
-        apiNumber = Math.floor(Math.random() * 7);
+        apiNumber = Math.floor(Math.random() * 6) + 1;
     }
     else if(type == "species")
     {
-        apiNumber = Math.floor(Math.random() * 38);
+        apiNumber = Math.floor(Math.random() * 37) + 1;
     }
 
     // switch (type)
